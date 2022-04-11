7ef2c5c951ba6dea1f2e45a46da03da96952eac9
coder-pro10z <85899936+coder-pro10z@users.noreply.github.com>
2022-04-12 01:34:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const hex=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+const btn=document.getElementById("btn");
+const color=document.querySelector(".color");
+
+// btn.addEventListener("click",function(){
+//     //get random number btwn 0 and 3
+//     // const randomNumber=2
+//     const randomNumber=getRandomNumber();
+//     console.log(randomNumber);
+//     document.body.style.backgroundColor=colors[randomNumber];
+//     color.textContent=colors[randomNumber];
+
+// });
+
+// function getRandomNumber(){
+//      return Math.floor(Math.random()*colors.length);
+// };
+btn.addEventListener("click",function(){
+    let hexColor="#";
+    for(let i=0;i<6;i++){ getRandomNumber:Number
+        hexColor+=hex[getRandomNumber()];
+    }
+    color.textContent=hexColor;
+    document.body.style.backgroundColor=hexColor;
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*hex.length);
+}
